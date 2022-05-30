381cc14bf20f062fcc418c8c7da63196f83e6e29
matheustanaka <matheustanakanog@gmail.com>
2022-05-30 20:28:12 -0300

    StrictMode

--- no-frills-react/src/App.js
+++ no-frills-react/src/App.js
@@ -1,12 +1,16 @@
 import { render } from "react-dom";
+import { StrictMode } from "react";
+
 // import Pet from "./Pet";
 import { SearchParams } from "./components/SearchParams";
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
